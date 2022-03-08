65b63577c6c7ef482167b6238fa6154cdd436105
jiangyanyang <jiangyan.yang@shopee.com>
2022-03-08 13:23:38 +0800

    final version

--- driver_amount_allocator/driver_amount_allocator.py
+++ driver_amount_allocator/driver_amount_allocator.py
@@ -8,7 +8,6 @@ import os
 import sys
 import traceback
 
-
 dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(dir)
 
@@ -60,20 +59,20 @@ def check_driver2(rule, driver):
         return False
 
 
-def amount_allocate(row, bill_no, driver_amount, driver2_amount, driver, driver2=None, assis1=None, assis2=None,
+def amount_allocate(bill_no, driver_amount, driver2_amount, driver, driver2=None, assis1=None, assis2=None,
                     assis3=None, assis4=None, assis5=None, assis6=None):
     # 参数类型/非空校验
     if not bill_no or not driver:
-        raise ValueError("第%s行参数错误：单据号不能为空！" % row)
+        raise ValueError("Excel内容异常：单据号不能为空！")
 
     if not driver_amount:
-        raise ValueError("第%s行参数错误：请检查excel中的场景是否存在，当前场景计算出的补贴金额=0！" % row)
+        raise ValueError("Excel内容异常：请检查单据号【%s】的场景是否存在，当前场景计算出的补贴金额=0！" % bill_no)
 
     try:
         driver_amount = float(driver_amount)
         driver2_amount = float(driver2_amount)
     except:
-        raise TypeError("Error input(row %s): driver_amount or driver2_amount should be decimal!" % row)
+        raise TypeError("Error input(bill_no %s): driver_amount or driver2_amount should be decimal!" % bill_no)
 
     driver_allocate_roles = {}
     if driver:
@@ -148,14 +147,29 @@ def data_deduplicate(data_frame):
     2. select 单据号 from xx group by 单据号 having count(1)>0，校验是否有1个单据号对应多条不同结果的记录，有则打印
     '''
     try:
-        # 整表去重
+        # ====== [第一步] 客户名称处理 ======
+        # 1. 获取客户名称包含葫芦娃的列，series
+        filter_1 = data_frame['客户名称'].str.contains(r".*葫芦娃.*")
+        # 2. 新增一列tmp, 如果客户名称包含葫芦娃则为1
+        data_frame["tmp"] = np.where(filter_1, 1, 0)
+
+        # 3. 按单据号分组，取tmp的sum值，dataframe
+        aggr_sum = data_frame.groupby("单据号").agg({"tmp": sum})
+        # 4. 过滤出sum>=1的单据号，series
+        filter_2 = aggr_sum[aggr_sum["tmp"] >= 1]
+        # 5. 上述series取index, 如果单据号在这个index list里，则客户名称改为葫芦娃，否则改为其他
+        data_frame["客户名称"] = np.where(data_frame["单据号"].isin(filter_2.index), "葫芦娃", "其他")
+        # 6. 去掉tmp列
+        data_frame = data_frame.drop('tmp', axis=1)
+
+        # ====== [第二步] 整表去重 ======
         data_frame = data_frame.drop_duplicates()
-        # 检查是否有异常记录
+        # ====== [第三步] 检查是否有异常记录 ======
         key_cnt = data_frame.groupby("单据号").size()  # group by 单据号
-        if len(key_cnt[key_cnt > 1]):                 # having count(1)>1
-            tmp = key_cnt[key_cnt > 1]                # 返回的是Series列表，直接打印的话最底下有一行name/dtype影响阅读
+        if len(key_cnt[key_cnt > 1]):  # having count(1)>1
+            tmp = key_cnt[key_cnt > 1]  # 返回的是Series列表，直接打印的话最底下有一行name/dtype影响阅读
             tmp_res = pd.DataFrame({'单据号': tmp.index, '派送次数': tmp.values})  # 因此转成dataframe, 给size列增加名字
-            print("如下单据号存在多条派送记录，可能存在重复计算，请检查！\n%s" % tmp_res.to_string(index=False))  # 打印时去掉最左侧的默认索引0123
+            print("Excel内容异常：如下单据号可能存在多条派送记录，请检查！\n%s" % tmp_res.to_string(index=False))  # 打印时去掉最左侧的默认索引0123
     except:
         print("Something error: ", traceback.format_exc())
     finally:
@@ -171,81 +185,69 @@ def main(rule_file, data_file, result_file):
         .to_dict(orient="records")
 
     drive_bill_raw = pd.read_excel(data_file,
-                              usecols=["单据号", "车牌号", "客户名称", "驾驶员2", "送书重量", "回头车拉货", "驾驶员",
-                                       "跟车员1", "跟车员2", "跟车员3", "跟车员4", "跟车员5", "跟车员6"],
-                              dtype={"送书重量": float}
-                              ).dropna(subset=["车牌号"]).fillna({"送书重量": 0}).fillna("")
-    # 客户名称处理
-    filter_1 = drive_bill_raw['客户名称'].str.contains(r".*葫芦娃.*")   # 获取客户名称包含葫芦娃的列
-    drive_bill_raw["tmp"] = np.where(filter_1, 1, 0)                 # 新增一列tmp, 如果客户名称包含葫芦娃则为1
-
-    filter_2 = drive_bill_raw.groupby("单据号").agg({"tmp":sum})
-    # print(filter_2[filter_2["tmp"]>=1])
-    # drive_bill_raw["客户名称_new"] = np.where(filter_2["tmp"]>=1, "葫芦娃", "其他")
-    drive_bill_raw["客户名称_new"] = np.where(drive_bill_raw[filter_2["tmp"]>=1], "葫芦娃", "其他")
-    print(drive_bill_raw)
-
+                                   usecols=["单据号", "车牌号", "客户名称", "驾驶员2", "送书重量", "回头车拉货", "驾驶员",
+                                            "跟车员1", "跟车员2", "跟车员3", "跟车员4", "跟车员5", "跟车员6"],
+                                   dtype={"送书重量": float}
+                                   ).dropna(subset=["车牌号"]).fillna({"送书重量": 0}).fillna("")
     drive_bill = data_deduplicate(data_frame=drive_bill_raw).to_dict(orient="records")
-    #
-    # final_result_list = []
-    # for data_row in range(len(drive_bill)):
-    #     fail_num = 0
-    #     for rule in rule_list:
-    #         data = drive_bill[data_row]
-    #         # print(fail_num, check_client_name(rule["客户名称"], data["客户名称"]),check_back_car(rule["回头车拉货"], data["回头车拉货"]),check_weight(rule["送书重量"], data["送书重量"]),check_driver2(rule["驾驶员2"], data["驾驶员2"]))
-    #         if rule["车牌号"] in data["车牌号"] \
-    #                 and check_client_name(rule["客户名称"], data["客户名称"]) \
-    #                 and check_back_car(rule["回头车拉货"], data["回头车拉货"]) \
-    #                 and check_weight(rule["送书重量"], data["送书重量"]) \
-    #                 and check_driver2(rule["驾驶员2"], data["驾驶员2"]):
-    #
-    #             total_driver_amount = rule["车牌补贴"] + rule["葫芦娃补贴"] + rule["回头车补贴"] + float(rule["重量(单价/吨)"]) * float(
-    #                 data["送书重量"])
-    #             total_driver2_amount = rule["驾驶员2补贴"]
-    #
-    #             try:
-    #                 single_record_result = amount_allocate(row=data_row + 1,
-    #                                                        bill_no=data["单据号"],
-    #                                                        driver_amount=total_driver_amount,
-    #                                                        driver2_amount=total_driver2_amount,
-    #                                                        driver=data["驾驶员"],
-    #                                                        driver2=data["驾驶员2"],
-    #                                                        assis1=data["跟车员1"],
-    #                                                        assis2=data["跟车员2"],
-    #                                                        assis3=data["跟车员3"],
-    #                                                        assis4=data["跟车员4"],
-    #                                                        assis5=data["跟车员5"],
-    #                                                        assis6=data["跟车员6"]
-    #                                                        )
-    #             except Exception as e:
-    #                 print(e)
-    #             else:
-    #                 final_result_list += single_record_result
-    #             finally:
-    #                 break
-    #         else:
-    #             fail_num += 1
-    #         if fail_num >= 72:
-    #             # if 6<=fail_num <= 7:
-    #             print("-----fail, scenario does not exist----", fail_num)
-    #             print(data)
-    #             print("-----fail----")
-    #
-    # final_result = pd.DataFrame(final_result_list)
-    # grouped = final_result.groupby("姓名").agg({"补贴金额": "sum"})
-    #
-    # with pd.ExcelWriter(result_file) as writer:
-    #     final_result.to_excel(writer, sheet_name='补贴明细', index=False)
-    #     grouped.to_excel(writer, sheet_name='金额合计')
+
+    final_result_list = []
+    for data in drive_bill:
+        fail_num = 0
+        for rule in rule_list:
+            # print(fail_num, check_client_name(rule["客户名称"], data["客户名称"]),check_back_car(rule["回头车拉货"], data["回头车拉货"]),check_weight(rule["送书重量"], data["送书重量"]),check_driver2(rule["驾驶员2"], data["驾驶员2"]))
+            if rule["车牌号"] in data["车牌号"] \
+                    and check_client_name(rule["客户名称"], data["客户名称"]) \
+                    and check_back_car(rule["回头车拉货"], data["回头车拉货"]) \
+                    and check_weight(rule["送书重量"], data["送书重量"]) \
+                    and check_driver2(rule["驾驶员2"], data["驾驶员2"]):
+
+                total_driver_amount = rule["车牌补贴"] + rule["葫芦娃补贴"] + rule["回头车补贴"] + float(rule["重量(单价/吨)"]) * float(
+                    data["送书重量"])
+                total_driver2_amount = rule["驾驶员2补贴"]
+
+                try:
+                    single_record_result = amount_allocate(bill_no=data["单据号"],
+                                                           driver_amount=total_driver_amount,
+                                                           driver2_amount=total_driver2_amount,
+                                                           driver=data["驾驶员"],
+                                                           driver2=data["驾驶员2"],
+                                                           assis1=data["跟车员1"],
+                                                           assis2=data["跟车员2"],
+                                                           assis3=data["跟车员3"],
+                                                           assis4=data["跟车员4"],
+                                                           assis5=data["跟车员5"],
+                                                           assis6=data["跟车员6"]
+                                                           )
+                except Exception as e:
+                    print(e)
+                else:
+                    final_result_list += single_record_result
+                finally:
+                    break
+            else:
+                fail_num += 1
+            if fail_num >= 72:
+                # if 6<=fail_num <= 7:
+                print("-----fail, scenario does not exist----", fail_num)
+                print(data)
+                print("-----fail----")
+
+    final_result = pd.DataFrame(final_result_list)
+    grouped = final_result.groupby("姓名").agg({"补贴金额": "sum"})
+
+    with pd.ExcelWriter(result_file) as writer:
+        final_result.to_excel(writer, sheet_name='补贴明细', index=False)
+        grouped.to_excel(writer, sheet_name='金额合计')
 
 
 if __name__ == "__main__":
     now = time.strftime('%Y%m%d_%H_%M_%S', time.localtime(int(time.time())))
 
     rule_file = dir + r'/规则场景_可改绿色格子内容.xlsx'
-    data_file = dir + r'/派车单明细_文件名不可更改.xlsx'
+    data_file = dir + r'/派车单明细.xlsx'
     result_file = dir + r'/统计结果_%s.xlsx' % now
 
     main(rule_file, data_file, result_file)
-    # print("\n计算完成，结果见文件【%s】\n弹窗1分钟后自动关闭，也可手动关闭~" % result_file)
-    # time.sleep(60)
+    print("\n计算完成，结果见文件【%s】\n弹窗1分钟后自动关闭，也可手动关闭~" % result_file)
+    time.sleep(60)
